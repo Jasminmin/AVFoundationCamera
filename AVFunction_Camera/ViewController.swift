68caf14142becdf977996b789fd578366864b397
Rebecca <rebecca9483@gmail.com>
2019-10-08 14:02:06 +0800

    Photo Capture

--- AVFunction_Camera/ViewController.swift
+++ AVFunction_Camera/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cameraController = CameraController()
+    
+    @IBOutlet fileprivate var captureButton: UIButton!
+    @IBOutlet fileprivate var photoModeButton: UIButton!
+    @IBOutlet fileprivate var videoModeButton: UIButton!
+    @IBOutlet fileprivate var toggleFlashButton: UIButton!
+    @IBOutlet fileprivate var toggleCameraButton: UIButton!
+    @IBOutlet fileprivate var capturePreviewView: UIView!
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        func configureCameraController() {
+            cameraController.prepare {(error) in if let error = error {
+                print(error)
+            }
+            try? self.cameraController.displayPreview(on: self.capturePreviewView)
+            }
+        }
+    
+        func styleCaptureButton() {
+            captureButton.layer.borderColor = UIColor(red: 77/255, green: 105/255, blue: 149/255, alpha: 1).cgColor
+            captureButton.layer.borderWidth = 3
+            captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+        }
+        styleCaptureButton()
+        configureCameraController()
     }
-
 
 }
 
