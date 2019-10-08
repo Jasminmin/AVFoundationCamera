829a725f5fe16d37db89b6208b355791efce15d9
Rebecca <rebecca9483@gmail.com>
2019-10-08 08:17:34 +0800

    Initial Commit

---
+++ AVFunction_Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFunction_Camera
+//
+//  Created by Rebecca on 2019/10/8.
+//  Copyright © 2019 Jasmine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
